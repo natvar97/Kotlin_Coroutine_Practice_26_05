017bc9a643eed511898e9d6ae512fa87be7fee16
Natavar Prajapati <prajapatinatavar21197@gmail.com>
2021-05-26 13:34:12 +0530

    main activity changes

--- app/src/main/java/com/example/kotlin_coroutine_practice_26_05/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_coroutine_practice_26_05/MainActivity.kt
@@ -1,24 +1,21 @@
 package com.example.kotlin_coroutine_practice_26_05
 
 import android.os.Bundle
-import android.provider.Settings
 import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.*
-import java.lang.Exception
 import kotlin.coroutines.CoroutineContext
-import kotlin.coroutines.coroutineContext
 
-class MainActivity : AppCompatActivity() , CoroutineScope{
+class MainActivity : AppCompatActivity(), CoroutineScope {
 
     private lateinit var textUser: TextView
     private lateinit var textMovie: TextView
-    private lateinit var job : Job
-    private lateinit var handler : CoroutineExceptionHandler
+    private lateinit var job: Job
+    private lateinit var handler: CoroutineExceptionHandler
 
     override val coroutineContext: CoroutineContext
-        get() = Dispatchers.Main + job + handler
+        get() = Dispatchers.Main + job
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,35 +26,17 @@ class MainActivity : AppCompatActivity() , CoroutineScope{
 
         job = Job()
 
-        //this is an activity scope not application scope , for application scope use GlobalScope.launch
-
-        //Exception handling in Kotlin coroutines
-
-    /*
-        //try and catch method
-        GlobalScope.launch(Dispatchers.Main) {
+        launch {
             try {
-                val user = async(Dispatchers.IO) { fetchUser() }
-                val movie = async(Dispatchers.IO) { fetchMovie() }
-                showResult(user.await(), movie.await())
-            } catch (e : Exception) {
-                e.printStackTrace()
+                coroutineScope {
+                    val differedUser = async { fetchUser() }
+                    val differedMovie = async { fetchMovie() }
+                    showResult(differedUser.await(), differedMovie.await())
+                }
+            } catch (e: Exception) {
             }
         }
 
-     */
-
-        //exception handling using CoroutineExceptionHandler
-
-        handler = CoroutineExceptionHandler{ _ , exception ->
-            Log.e("tag exception" , "$exception handled")
-        }
-
-        GlobalScope.launch(Dispatchers.Main + handler) {
-            val user = async(Dispatchers.IO) { fetchUser() }
-            val movie = async(Dispatchers.IO) { fetchMovie() }
-            showResult(user.await(), movie.await())
-        }
 
     }
 
@@ -83,8 +62,6 @@ class MainActivity : AppCompatActivity() , CoroutineScope{
         textMovie.text = "Movie name : ${movie.name} , rating : ${movie.rating}"
     }
 
-//    }
-
     suspend fun fetchMovie(): Movie {
         return withContext(Dispatchers.IO) {
             val movie = Movie()
@@ -99,55 +76,76 @@ class MainActivity : AppCompatActivity() , CoroutineScope{
 
 }
 
-        /*
+/*
+        * Personal try and catch
 
-        /*
-            //try and catch method
-            GlobalScope.launch(Dispatchers.Main) {
+            launch {
                 try {
-                    val user = async(Dispatchers.IO) { fetchUser() }
-                    val movie = async(Dispatchers.IO) { fetchMovie() }
-                    showResult(user.await(), movie.await())
-                } catch (e : Exception) {
+                    val user = try {
+                        fetchUser()
+                    } catch (e : Exception) {
+
+                    }
+                    val movie = try {
+                        fetchMovie()
+                    } catch (e : Exception) {
+
+                    }
+                    showResult(user as User , movie as Movie)
+
+                } catch (e: Exception) {
                     e.printStackTrace()
                 }
             }
 
-         */
+ */
 
-            //exception handling using CoroutineExceptionHandler
+/*
 
-            val handler = CoroutineExceptionHandler{ _ , exception ->
-                Log.e("tag exception" , "$exception handled")
-            }
+/*
+    //try and catch method
+    GlobalScope.launch(Dispatchers.Main) {
+        try {
+            val user = async(Dispatchers.IO) { fetchUser() }
+            val movie = async(Dispatchers.IO) { fetchMovie() }
+            showResult(user.await(), movie.await())
+        } catch (e : Exception) {
+            e.printStackTrace()
+        }
+    }
 
-            GlobalScope.launch(Dispatchers.Main + handler) {
-                val user = async(Dispatchers.IO) { fetchUser() }
-                val movie = async(Dispatchers.IO) { fetchMovie() }
-                showResult(user.await(), movie.await())
-            }
+ */
+
+    //exception handling using CoroutineExceptionHandler
+
+    val handler = CoroutineExceptionHandler{ _ , exception ->
+        Log.e("tag exception" , "$exception handled")
+    }
+
+    GlobalScope.launch(Dispatchers.Main + handler) {
+        val user = async(Dispatchers.IO) { fetchUser() }
+        val movie = async(Dispatchers.IO) { fetchMovie() }
+        showResult(user.await(), movie.await())
+    }
 
 
 
-         */
+ */
 
 
+/*
+    * if we need global scope which is our application scope, not an activity scope then use it like below
+        GlobalScope.launch(Dispatchers.Main) {
+            val user = async(Dispatchers.IO) { fetchUser() }
+            val movie = async(Dispatchers.IO) { fetchMovie() }
+            showResult(user.await(), movie.await())
+        }
 
+        **notes** :- using Global scope which is our application scope
+                       if we destroy the activity , then it continue running fetchUser() and fetchMovie()
+                       functions as we use GlobalScope
 
-        /*
-            * if we need global scope which is our application scope, not an activity scope then use it like below
-                GlobalScope.launch(Dispatchers.Main) {
-                    val user = async(Dispatchers.IO) { fetchUser() }
-                    val movie = async(Dispatchers.IO) { fetchMovie() }
-                    showResult(user.await(), movie.await())
-                }
-
-                **notes** :- using Global scope which is our application scope
-                               if we destroy the activity , then it continue running fetchUser() and fetchMovie()
-                               functions as we use GlobalScope
-
-         */
-
+ */
 
 
 /*
